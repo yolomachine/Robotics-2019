7f3ac131512f97764612bcba0bd1465060a516c6
kutselabskii <osutanfirst@gmail.com>
2019-11-17 03:46:56 +1000

    Implement line following

--- main_sketch/main_sketch.ino
+++ main_sketch/main_sketch.ino
@@ -1,28 +1,50 @@
+#include <SparkFun_TB6612.h>
 #include <IRremoteInt.h>
 #include <IRremote.h>
+#include <Servo.h>
 
 #define S0 11
 #define S1 10
 #define S2 8
 #define S3 7
 #define sensorOut 12
+
 #define RECV_PIN 2
+
+#define LINE_PIN_LEFT A4
+#define LINE_PIN_RIGHT A5
+#define SERVO_PIN 6
+
+#define DRIVER_IN1 5
+#define DRIVER_IN2 4
+#define DRIVER_PWM 3
+#define DRIVER_STANDBY 9
 
 int frequency = 0;
 IRrecv irrecv(RECV_PIN);
 uint8_t locations[8];
 
+Motor motor = Motor(DRIVER_IN1, DRIVER_IN2, DRIVER_PWM, -1, DRIVER_STANDBY);
+Servo servo;
+
 void setup() {
-  pinMode(S0, OUTPUT);
-  pinMode(S1, OUTPUT);
-  pinMode(S2, OUTPUT);
-  pinMode(S3, OUTPUT);
-  pinMode(sensorOut, INPUT);
-  irrecv.enableIRIn();
-  digitalWrite(S0,HIGH);
-  digitalWrite(S1,LOW);
+  //pinMode(S0, OUTPUT);
+  //pinMode(S1, OUTPUT);
+  //pinMode(S2, OUTPUT);
+  //pinMode(S3, OUTPUT);
+  //pinMode(sensorOut, INPUT);
+  //irrecv.enableIRIn();
+  //digitalWrite(S0,HIGH);
+  //digitalWrite(S1,LOW);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LINE_PIN_LEFT, INPUT);
+  pinMode(LINE_PIN_RIGHT, INPUT);
+  servo.attach(SERVO_PIN);
   
   Serial.begin(9600);
+
+  delay(3000);
 }
 
 bool locations_was_recieved = false;
@@ -75,35 +97,57 @@ bool try_recieve_locations() {
   return false;
 }
 
+String getColor() {
+  //  digitalWrite(S2,LOW);
+  //  digitalWrite(S3,LOW);
+  //  frequency = pulseIn(sensorOut, LOW);
+  //  frequency = map(frequency, 550,1050,255,0);
+  //
+  //  Serial.print("R= ");
+  //  Serial.print(frequency);
+  //  Serial.print("  ");
+  //
+  //  digitalWrite(S2,HIGH);
+  //  digitalWrite(S3,HIGH);
+  //  frequency = pulseIn(sensorOut, LOW);
+  //  frequency = map(frequency, 550,1050,255,0);
+  //
+  //  Serial.print("G= ");
+  //  Serial.print(frequency);
+  //  Serial.print("  ");
+  //
+  //  digitalWrite(S2,LOW);
+  //  digitalWrite(S3,HIGH);
+  //  frequency = pulseIn(sensorOut, LOW);
+  //  frequency = map(frequency, 550,1050,255,0);
+  //
+  //  Serial.print("B= ");
+  //  Serial.print(frequency);
+  //  Serial.println("  ");
+}
+
+
+
 void loop() {
-  if (!locations_was_recieved && !(locations_was_recieved = try_recieve_locations())) {
-    Serial.println("still getting info");
-    return;
+  //if (!locations_was_recieved && !(locations_was_recieved = try_recieve_locations())) {
+  //  return;
+  //}
+  
+  //motor.drive(125, 2000);
+  //motor.brake();
+  //delay(10000);
+
+  int blackLeft = digitalRead(LINE_PIN_LEFT);
+  int blackRight = digitalRead(LINE_PIN_RIGHT);
+
+  if (!blackLeft && !blackRight) {
+    servo.write(90);
+  } else {
+    if (blackLeft) {
+      servo.write(70);
+    } else {
+      servo.write(110);
+    }
   }
-//  digitalWrite(S2,LOW);
-//  digitalWrite(S3,LOW);
-//  frequency = pulseIn(sensorOut, LOW);
-//  frequency = map(frequency, 550,1050,255,0);
-//
-//  Serial.print("R= ");
-//  Serial.print(frequency);
-//  Serial.print("  ");
-//
-//  digitalWrite(S2,HIGH);
-//  digitalWrite(S3,HIGH);
-//  frequency = pulseIn(sensorOut, LOW);
-//  frequency = map(frequency, 550,1050,255,0);
-//
-//  Serial.print("G= ");
-//  Serial.print(frequency);
-//  Serial.print("  ");
-//
-//  digitalWrite(S2,LOW);
-//  digitalWrite(S3,HIGH);
-//  frequency = pulseIn(sensorOut, LOW);
-//  frequency = map(frequency, 550,1050,255,0);
-//
-//  Serial.print("B= ");
-//  Serial.print(frequency);
-//  Serial.println("  ");
+  motor.drive(125);
 }
